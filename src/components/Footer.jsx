3c461acfaa555c58031a1156dd719dc32fed08f5
Nirma5754 <raj0307patil@gmail.com>
2026-07-07 10:00:58 +0530

    raj

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-10 font-mono">
-      <p className="text-center text-emerald-400/60 text-xs">© 2025 Arnav Panchal. All rights reserved.</p>
+      <p className="text-center text-emerald-400/60 text-xs">© 2025 Nirmal Patil. All rights reserved.</p>
     </footer>
   )
 }
